3ab6c96786b040566cf3403a89b8bbab8485a45e
amanpages <151524274+amanpages@users.noreply.github.com>
2024-08-23 12:03:58 +0530

    Update apiService.js

--- shopify_dashboard/frontend/src/services/apiService.js
+++ shopify_dashboard/frontend/src/services/apiService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Define the base URL for the API
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://dashboard-rapidquest.onrender.com';
 
 // Create an Axios instance with the base URL
 const apiClient = axios.create({
